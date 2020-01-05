e923fa6c5d1c283f673a86887e2070bd1ca1ca12
mazuecos3 <oscar3mazuecos@hotmail.com>
2020-01-05 07:46:54 +0100

    Cambiendo para que heroku elija puerto

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+let puerto = process.env.PORT;
+
 // Paginas publicas (estaticas)
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Escuchemos en un puerto
-app.listen(3000, () => {
+app.listen(puerto, () => {
     console.log(" * Miniserver UP and Running en http://localhost:3000");
 });
